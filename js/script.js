9e1a477f4edbaed8b408a50803b51610dd269660
Zulfikar <vonagrut@gmail.com>
2020-08-26 18:09:29 +0600

    reg_exp

--- js/script.js
+++ js/script.js
@@ -379,10 +379,10 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
 
     nextButton.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if (offset == +width.replace(/\D/g, '') * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += +width.replace(/\D/g, '');
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
         if (slideIndex == slides.length) {
@@ -403,9 +403,9 @@ window.addEventListener('DOMContentLoaded', () =>{
 
     prevButton.addEventListener('click', () => {
         if (offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = +width.replace(/\D/g, '') * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= +width.replace(/\D/g, '');
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -430,7 +430,7 @@ window.addEventListener('DOMContentLoaded', () =>{
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = +width.replace(/\D/g, '') * (slideTo - 1);
             slidesField.style.transform = `translateX(-${offset}px)`;
 
             dots.forEach(dot => dot.style.opacity = '0.5');
