386e384eb307f7fbf08c53ebd3f576369b0ae97b
Зульфикар Турганов <vonagrut@mbp-zulfikar.zte.com.cn>
2020-10-02 14:20:05 +0600

    lesson_encapsulation

---
+++ lesson_encapsulation/encapsulation.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Petrychenko';
+
+    say = () => {
+        console.log(`Имя: ${this.name} ${this.#surname}, Возраст: ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Некорректные данные!');
+        }
+    }
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.age)
+ivan.age = 30
+console.log(ivan.age)
